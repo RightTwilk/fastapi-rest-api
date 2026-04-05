f0ff4be63048a9846f36769dd3da6bae5f7379da
Danila Zmitser <ubisofter506@gmail.com>
2026-04-05 13:33:46 +0300

    Some changes and black formating

---
+++ models/items/items_models.py
@@ -0,0 +1,20 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
+from typing import Optional
+from sqlalchemy import Date, Numeric, Enum
+from models.items.lang_enum import Language
+from decimal import Decimal
+
+
+class Item(DeclarativeBase):
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower()
+
+    __abstract__ = True
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(nullable=False)
+    publication_year: Mapped[Date]
+    publisher: Mapped[str] = mapped_column(nullable=False)
+    language: Mapped[Language] = mapped_column(Enum(Language), nullable=False)
+    price: Mapped[Optional[Decimal]] = mapped_column(Numeric(10, 2), nullable=False)
---
+++ models/items/lang_enum.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class Language(Enum):
+    ENGLISH = "en"
+    RUSSIAN = "ru"
+    FRENCH = "fr"
+    GERMAN = "de"
+    SPANISH = "es"
+    ITALIAN = "it"
+    CHINESE = "zh"
+    JAPANESE = "ja"
+    ARABIC = "ar"
+    PORTUGUESE = "pt"
