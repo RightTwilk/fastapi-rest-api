c69653f2c6d7d342046460ca329afd0a0286faf2
Danila Zmitser <ubisofter506@gmail.com>
2026-04-06 12:31:20 +0300

    Chenges in class namings, gitignore, pydantic schemas, checkers for db

--- models/items_models_package/books_models_package/books_models.py
+++ models/items_models_package/books_models_package/books_models.py
@@ -1,12 +1,12 @@
 from sqlalchemy.orm import Mapped, mapped_column
-from models.items_models_package.items_models import Item
-from models.items_models_package.books_models_package.genre_enum import BookGenre
-from models.items_models_package.books_models_package.cover_enum import CoverType
+from models.items_models_package.items_models import ItemModel
+from enums.genre_enum import BookGenre
+from enums.cover_enum import CoverType
 
 from sqlalchemy import Enum
 
 
-class Book(Item):
+class Book(ItemModel):
     author: Mapped[str] = mapped_column(nullable=False)
     isbn: Mapped[str] = mapped_column(nullable=False)
     page_count: Mapped[int]
--- models/items_models_package/items_models.py
+++ models/items_models_package/items_models.py
@@ -1,11 +1,11 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
 from typing import Optional
 from sqlalchemy import Numeric, Enum
-from models.items_models_package.lang_enum import Language
+from enums.lang_enum import Language
 from decimal import Decimal
 
 
-class Item(DeclarativeBase):
+class ItemModel(DeclarativeBase):
 
     @declared_attr.directive
     def __tablename__(cls) -> str:
@@ -14,7 +14,7 @@ class Item(DeclarativeBase):
     __abstract__ = True
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(nullable=False)
-    publication_year: Mapped[str]
+    publication_year: Mapped[int]
     publisher: Mapped[str] = mapped_column(nullable=False)
     language: Mapped[Language] = mapped_column(Enum(Language), nullable=False)
     price: Mapped[Optional[Decimal]] = mapped_column(Numeric(10, 2), nullable=False)
