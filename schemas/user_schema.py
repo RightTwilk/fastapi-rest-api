b1a2ada3a961c87ff1470066efc2a2f69201a496
Danila Zmitser <ubisofter506@gmail.com>
2026-04-11 18:35:47 +0300

    Created pydantic schema for user

---
+++ schemas/user_schema.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, Field, EmailStr
+from typing import Optional
+
+
+class UserSchema(BaseModel):
+    username: str = Field(..., max_length=50)
+    email: EmailStr = Field(...)
+    full_name: Optional[str] = Field(max_length=50)
+    password: str = Field(...)
+    
