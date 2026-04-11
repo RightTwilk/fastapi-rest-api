e44c47d47b418695a03a101358dbd319600f6356
Danila Zmitser <ubisofter506@gmail.com>
2026-04-11 18:32:15 +0300

    Created model of user

---
+++ models/users_models.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import Mapped, DeclarativeBase, mapped_column
+from datetime import datetime
+from typing import Optional
+
+
+
+class UserModel(DeclarativeBase):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    email: Mapped[str] = mapped_column(unique=True, nullable=False, index=True)
+    username: Mapped[str] = mapped_column(unique=True, nullable=False, index=True)
+    full_name: Mapped[Optional[str]] 
+    hashed_password: Mapped[str] = mapped_column(nullable=False)
+    is_active: Mapped[bool] = mapped_column(default=True)
+    is_superuser: Mapped[bool] = mapped_column(default=False)
+    created_at: Mapped[datetime] = mapped_column(default=datetime.now())
+    updated_at: Mapped[datetime] = mapped_column(default=datetime.now())
