f6a78df3dea39a933665b7f0a29b3f55160fa3e7
Danila Zmitser <ubisofter506@gmail.com>
2026-04-21 16:44:42 +0300

    Deleted comments

--- src/app/items/enums/cover_enum.py
+++ src/app/items/enums/cover_enum.py
@@ -2,10 +2,10 @@ from enum import Enum
 
 
 class CoverType(str, Enum):
-    HARDCOVER = "hardcover"  # Твёрдый переплёт
-    SOFTCOVER = "softcover"  # Мягкая обложка
-    DUST_JACKET = "dust_jacket"  # Суперобложка
-    LEATHER = "leather"  # Кожаный переплёт
-    PAPERBACK = "paperback"  # Бумажная обложка (часто синоним softcover)
-    SPIRAL = "spiral"  # Спиральный переплёт
-    BOARD_BOOK = "board_book"  # Картонная обложка (для детских книг)
+    HARDCOVER = "hardcover"  
+    SOFTCOVER = "softcover"  
+    DUST_JACKET = "dust_jacket" 
+    LEATHER = "leather"  
+    PAPERBACK = "paperback"  
+    SPIRAL = "spiral"  
+    BOARD_BOOK = "board_book"  
