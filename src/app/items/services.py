b0a94648d1c5ef16ce9785dd9523b228e43f348c
Danila Zmitser <ubisofter506@gmail.com>
2026-04-23 12:24:17 +0300

    add crud for creating string in db

--- src/app/items/services.py
+++ src/app/items/services.py
@@ -2,11 +2,22 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from sqlalchemy.engine import Result
 from app.items.models import Book
+from app.items.schemas import BookCreate
 
 
-#books
+# books
 async def get_book(session: AsyncSession) -> list[Book]:
     result: Result[tuple[Book]] = await session.execute(select(Book).order_by(Book.id))
     return list(result.scalars().all())
+
+
 async def get_book_by_id(session: AsyncSession, book_id: int) -> Book | None:
     return await session.get(Book, book_id)
+
+
+async def create_book(session: AsyncSession, book_data: BookCreate) -> Book:
+    book_model = Book(**book_data.model_dump())
+    session.add(book_model)
+    await session.commit()
+    await session.refresh(book_model)
+    return book_model
