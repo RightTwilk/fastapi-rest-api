518530bfd14335b2d8088fca25d1034ef5a11a4b
Danila Zmitser <ubisofter506@gmail.com>
2026-04-29 13:52:59 +0300

    Make router for getting book and tracking mistakes

--- src/app/items/router.py
+++ src/app/items/router.py
@@ -1,16 +1,42 @@
-from fastapi import APIRouter, Depends
-from app.items.schemas import BookCreate
-from app.items.services import create_book
+from fastapi import APIRouter, Depends, HTTPException, status
+from app.items.schemas import (
+    BookBase,
+    BookCreate,
+)
+from app.items.services import create_book, get_book, get_book_by_id
 from sqlalchemy.ext.asyncio import AsyncSession
 from core.database import session_maker
+from app.items.models import Book
+
+item_router = APIRouter(tags=["Items"], prefix="/item")
 
 
-item_router = APIRouter(tags=["Items"], prefix="/items")
+# book views
+book_router = APIRouter(tags=["Book"], prefix="/book")
 
 
-@item_router.post("create_book/", response_model=None)
+@book_router.post("create_book/", response_model=BookCreate)
 async def create_book_view(
     book: BookCreate, session: AsyncSession = Depends(session_maker)
 ):
     await create_book(session=session, book_data=book)
-    return {'BookCreate':'Succes'}
+    return {"BookCreate": "Succes"}
+
+
+@book_router.get("get_book/", response_model=list[Book])
+async def get_book_view(
+    book: BookBase, session: AsyncSession = Depends(session_maker)
+) -> list[Book]:
+    return await get_book(session=session)
+
+
+@book_router.get("get_book_by_id/{book_id}", response_model=BookBase | None)
+async def get_book_by_id_view(
+    book_id: int, session: AsyncSession = Depends(session_maker)
+) -> BookBase | None:
+    book = await get_book_by_id(session=session, book_id=book_id)
+    if book is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+        return None
+
+    return BookBase.model_validate(book)
